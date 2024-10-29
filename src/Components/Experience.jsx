14af3f67802d87e4c121cfa9212d052691edcad6
Brian <gameboy95thang@gmail.com>
2024-10-29 16:51:12 -0700

    yes

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -71,6 +71,7 @@ export default function Experience() {
     return(
         <div>
             <div>
+            <Divider variant="inset" component="li"/>
                 <h3>My Amazon Favorites</h3>
                 <center>
                     <div>
@@ -81,6 +82,7 @@ export default function Experience() {
                         <a href='https://amzn.to/4hpuqYo'>https://amzn.to/4hpuqYo</a>
                     </div>
                 </center>
+                <Divider variant="inset" component="li"/>
             </div>
             <div className="work">
                 <h3>My Work Experience</h3>
